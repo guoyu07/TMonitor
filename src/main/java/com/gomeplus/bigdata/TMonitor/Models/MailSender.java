de819b31ee98af80f8715ea4da496b0c903cadcc
周井江-技术中心-服务研发部 <zhoujingjiang@gomeplus.com>
2017-08-18 11:09:31 +0800

    refactor

--- src/main/java/com/gomeplus/bigdata/TMonitor/Models/MailSender.java
+++ src/main/java/com/gomeplus/bigdata/TMonitor/Models/MailSender.java
@@ -15,14 +15,36 @@ public final class MailSender {
     private String smtpPassword;
     private Session mailSession;
     private Transport transport;
+    private Properties properties;
 
     public MailSender() {
         this(null);
     }
 
     public MailSender(Properties properties) {
+        initialize(this.properties = properties);
+    }
+
+    private void initialize(Properties properties) {
         try {
-            initialize(properties);
+            if (properties == null) {
+                InputStream inputStream = MailSender.class
+                        .getResourceAsStream("/mail.properties");
+                properties = new Properties();
+                properties.load(inputStream);
+            }
+
+            smtpUserName = properties.getProperty("mail.smtp.user");
+            smtpPassword = properties.getProperty("mail.smtp.password");
+            String smtpHost = properties.getProperty("mail.smtp.host");
+            mailSession = Session.getInstance(properties, new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(smtpUserName, smtpPassword);
+                }
+            });
+            transport = mailSession.getTransport();
+            transport.connect(smtpHost, smtpUserName, smtpPassword);
         } catch (IOException ex) {
             ex.printStackTrace();
             log.error(ex.getMessage());
@@ -34,28 +56,6 @@ public final class MailSender {
         }
     }
 
-    private void initialize(Properties properties)
-            throws IOException, MessagingException {
-        if (properties == null) {
-            InputStream inputStream = MailSender.class
-                    .getResourceAsStream("/mail.properties");
-            properties = new Properties();
-            properties.load(inputStream);
-        }
-
-        smtpUserName = properties.getProperty("mail.smtp.user");
-        smtpPassword = properties.getProperty("mail.smtp.password");
-        String smtpHost = properties.getProperty("mail.smtp.host");
-        mailSession = Session.getInstance(properties, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(smtpUserName, smtpPassword);
-            }
-        });
-        transport = mailSession.getTransport();
-        transport.connect(smtpHost, smtpUserName, smtpPassword);
-    }
-
     public MailSender sendMessage(String toUsers, String subject,
                             String content, String contentType) throws MessagingException {
         MimeMessage message = new MimeMessage(mailSession);
@@ -65,7 +65,12 @@ public final class MailSender {
         if (contentType == null)
             contentType = "text/plain;charset=UTF-8";
         message.setContent(content, contentType);
-        transport.sendMessage(message, message.getAllRecipients());
+
+        try {
+            transport.sendMessage(message, message.getAllRecipients());
+        } catch (IllegalStateException ex) {
+            initialize(this.properties);
+        }
 
         return this;
     }
